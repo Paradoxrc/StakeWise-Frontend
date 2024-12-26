0d1cdc41203369954de8a7ee8799bacdd0da0b8f
HirunaWarusawithana <hirunawarusawithana@gmail.com>
2024-12-26 10:51:28 +0530

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Upcoming from "./pages/Upcoming/Upcoming";
+import Results from "./pages/Results/Results";
+import Politics from "./pages/Politics/Politics";
+import Sports from "./pages/Sports/Sports";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/politics" element={<Politics />} />
+          <Route path="/sports" element={<Sports />} />
+          <Route path="/upcoming" element={<Upcoming />} />
+          <Route path="/result" element={<Results />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/Navbar/Navbar";
 import Slider from "@/components/Slider/Slider";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import Card from "@/components/Card/Card";
@@ -6,7 +5,6 @@ import Card from "@/components/Card/Card";
 const HomePage = () => {
   return (
     <div className="bg-primary min-h-screen">
-      <Navbar />
       <Slider />
       <SearchBar />
       <Card />
